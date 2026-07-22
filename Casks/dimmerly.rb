75eb53f9b683bfdf1c6f6274fbcc20b8ec5b81d9
Zoran Olujic <olujicz@gmail.com>
2026-07-22 13:46:08 +0200

    Update Dimmerly to 1.1.1

--- Casks/dimmerly.rb
+++ Casks/dimmerly.rb
@@ -1,6 +1,6 @@
 cask "dimmerly" do
-  version "1.1.0"
-  sha256 "b57cf9b236dbacba8894216abad2ba5cd515b038ef6d475496323ec5c9e2c5d1"
+  version "1.1.1"
+  sha256 "5eecc3e7f177f5222225393f0fc6e7769043f714d62a356dd5caaf57c8b301c2"
 
   url "https://github.com/olujicz/Dimmerly/releases/download/v#{version}/Dimmerly-#{version}.dmg"
   name "Dimmerly"
