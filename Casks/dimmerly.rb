887126dbbfb349a54a25a813bdf22ae2313f7061
Zoran Olujic <olujicz@gmail.com>
2026-07-22 14:00:54 +0200

    Improve Dimmerly cask metadata

--- Casks/dimmerly.rb
+++ Casks/dimmerly.rb
@@ -7,7 +7,25 @@ cask "dimmerly" do
   desc "Menu bar display dimmer and brightness controller"
   homepage "https://github.com/olujicz/Dimmerly"
 
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  depends_on macos: :sequoia
+
   app "Dimmerly.app"
 
   uninstall quit: "rs.in.olujic.dimmerly"
+
+  zap trash: [
+    "~/Library/Application Scripts/MN5C3DH647.rs.in.olujic.dimmerly",
+    "~/Library/Application Scripts/rs.in.olujic.dimmerly",
+    "~/Library/Application Scripts/rs.in.olujic.dimmerly.DimmerlyWidget",
+    "~/Library/Containers/rs.in.olujic.dimmerly",
+    "~/Library/Containers/rs.in.olujic.dimmerly.DimmerlyWidget",
+    "~/Library/Group Containers/MN5C3DH647.rs.in.olujic.dimmerly",
+    "~/Library/Preferences/MN5C3DH647.rs.in.olujic.dimmerly.plist",
+    "~/Library/Preferences/rs.in.olujic.dimmerly.plist",
+  ]
 end
